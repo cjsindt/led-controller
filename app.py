f960b73adde7153cf6d4f1eb25e3a274358906f6
cjsindt <connor-sindt@uiowa.edu>
2021-06-27 02:18:40 -0500

    initial commit

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return "<html><body><img src=\"https://wpcdn.us-midwest-1.vip.tn-cloud.net/www.reptilesmagazine.com/content/uploads/data-import/6fd47469/tomato-frog11.jpg\" alt=\"yee-haw\"></img><div><h1>yee-haw</h1></div></body></html>"
+
+
+if __name__ == '__main__':
+    app.run()
